acca5ec8f34adf4bf5f676bfd9daa32a35f52e2f
alkor-realty-dev <113501097+alkor-realty-dev@users.noreply.github.com>
2022-10-10 17:14:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ try {
                     imageesArr = req?.body?.imageUrls ? req.body.imageUrls : false;
                     cdnInageArr = [];
 
-                    res.status(200).send({'sssss': 'ssss'});
+                    res.status(200).send({'sssss': req});
                     /*if (req.header('X-GET-302')) {
                         for (var i = 0; i < imageesArr.length; i++) {
                             var ingUrl = imageesArr[i], rawIOmgData, cdnImageUrlObj;
